07a7fdecfa8a29ba4c6ae07e9f2e2514b821c206
Wedeueis Braz <wedeueisbraz@yahoo.com.br>
2023-01-07 16:03:28 -0300

    env and config files

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = json.load(open(os.path.join(os.getenv("RL_PROJECT_ROOT"), "config.json")))
+
+project_root = os.getenv("RL_PROJECT_ROOT")
